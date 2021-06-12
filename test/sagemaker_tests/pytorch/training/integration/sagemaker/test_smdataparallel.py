e05ca8911ec1dbf6acfd85893ae820482ae3c61d
Mansi Mane <mansmane@amazon.com>
2021-06-11 18:10:53 -0700

    Minor fix

--- test/sagemaker_tests/pytorch/training/integration/sagemaker/test_smdataparallel.py
+++ test/sagemaker_tests/pytorch/training/integration/sagemaker/test_smdataparallel.py
@@ -11,7 +11,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
-
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = 1
 import pytest
 import os
 from sagemaker.pytorch import PyTorch
